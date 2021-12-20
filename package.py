d8fd6705de030dab45546cd098d49877e6efc8e5
Gopikaudayan <93866171+Gopikaudayan@users.noreply.github.com>
2021-12-20 12:54:49 +0530

    Add files via upload

---
+++ package.py
@@ -0,0 +1,9 @@
+from graphics import rec
+from graphics import cir
+l=int(input("enter the length of rectangle"))
+b=int(input("enter the breadth of rectangle"))
+rec.area(l,b)
+rec.perimeter(l,b)
+r=int(input("enter the radious"))
+cir.area(r)
+cir.perimeter(r)
