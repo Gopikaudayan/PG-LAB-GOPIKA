14d974142940fdafe39213228b4338f149aaca3a
Gopikaudayan <93866171+Gopikaudayan@users.noreply.github.com>
2022-01-29 20:34:02 +0530

    Add files via upload

---
+++ c02/co2_11.py
@@ -0,0 +1,7 @@
+import math
+t_area=lambda b,h:1/2*b*h
+r_area=lambda l,b:l*b
+s_area=lambda a:a*a
+print("area of triangle:",t_area(10,20))
+print("area of rectangle:",r_area(4,5))
+print("area of circle:",s_area(4))
